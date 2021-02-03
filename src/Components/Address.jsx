1730c021215d9a87fc77a7d9a8efb06606adeea7
Abishazaigo <you@example.com>
2021-02-03 10:37:21 +0530

    Component created for icons of address

---
+++ src/Components/Address.jsx
@@ -0,0 +1,11 @@
+function Address(props){
+    return(
+        <>
+        <p><i className="pas material-icons">{props.icon}</i>
+            <span class="pa">{props.value}</span>
+        </p>
+       
+        </>
+    )
+}
+export default Address;
