ffe14dea88fdb5feaa9808bab4969f3e8bc35a0a
Abishazaigo <you@example.com>
2021-02-03 10:38:48 +0530

    Component created for getting comment list

---
+++ src/Components/PostComment.jsx
@@ -0,0 +1,31 @@
+import {useState, useEffect} from "react";
+import axios from 'axios';
+
+function PostComment(){
+    const [postcomments, setPostComments] = useState([]);
+    useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/1/comments")
+      .then((res) => {
+        console.log(res.data);
+        setPostComments(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return(
+    postcomments.map((postcomment) => {
+      return (
+        <div className="comment">
+        <div className="card p-3">
+          <p>{postcomment.title}</p>
+          <p>{postcomment.body}</p>
+        </div>
+        </div>
+      );
+    })
+  )
+}
+export default PostComment;
