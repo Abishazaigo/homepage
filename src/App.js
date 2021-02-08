408109b4fe54e888a5342849fb8951b3b837c2cf
Abishazaigo <you@example.com>
2021-02-08 10:05:56 +0530

    Addded router and query client provider

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import logo from './logo.svg';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import Post from './Components/Post';
-import Comment from './Components/PostComment';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './components/home/Home';
+import Post from './components/home/Post';
+import PostComment from './components/home/PostComment';
+import Contact from './components/contact/Contact';
+import Load from './components/home/Load';
+import './styles/Web.css';
 function App() {
+  const queryClient = new QueryClient();
   return (
+    <QueryClientProvider client={queryClient}>
       <Router>
       <Switch>
       <Route exact path="/">
       <Home />
       </Route>
-      <Route exact path="/Post">
+      <Route exact path="/post">
       <Header />
       <Post />
       <Footer />
       </Route>
-      <Route exact path="/PostComment">
+      <Route exact path="/postcomment">
       <Header />
-      <Comment/>
+      <PostComment/>
       <Footer />
+      </Route>
+      <Route exact path="/contact">
+      <Contact />
+      </Route>
+      <Route exact path="/load">
+      <Load />
       </Route>
       </Switch>
       </Router>
+      </QueryClientProvider>
   )
 }
 
